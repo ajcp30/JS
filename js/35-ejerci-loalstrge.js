2025f9cf5546ab2bc145a705bc3e109873785b4d
Antonio <antonio@github.com>
2024-01-18 23:22:24 +0100

    update

--- js/35-ejerci-loalstrge.js
+++ js/35-ejerci-loalstrge.js
@@ -1,4 +1,34 @@
-'use strict'
+'use strict';
 
+var formulario = document.querySelector("#formpelicula");
 
-//
+formulario.addEventListener('submit', function(){
+
+    var titulo = document.querySelector('#addpelicula').value;
+    if(titulo.length >= 1){
+        localStorage.setItem(titulo, titulo);
+
+    }
+});
+
+var ul = document.querySelector('#lista-pelis');
+for(var i in localStorage){
+    if(typeof localStorage[i] == 'string'){
+        var li = document.createElement("li");
+        li.append(localStorage[i]);
+        ul.append(li);
+    }
+    
+}
+
+var formulariob = document.querySelector("#formBorrarPelicula");
+
+formulariob.addEventListener('submit', function(){
+
+    var titulo = document.querySelector('#borraPelicula').value;
+    
+    if(titulo.length >= 1){
+        localStorage.removeItem(titulo);
+
+    }
+});
