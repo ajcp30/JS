32402c59ea1847e169da1018398c2f4b1e6b2dfa
Antonio <antonio@github.com>
2024-02-25 23:32:39 +0100

    update content

--- Jquery/js/02-eventos.js
+++ Jquery/js/02-eventos.js
@@ -1,79 +1,69 @@
 $(document).ready(function(){
-    // mouseover y mouseout
-    var caja =  $("#caja");
-    /*
-    caja.mouseover(function(){
-        $(this).css("background","red");
-    });
 
-    caja.mouseout(function(){
-        $(this).css("background","orange");
-    });
-    */
+	// MouseOver y MouseOut
+	var caja = $("#caja");
 
-    function cambiaRojo(){
-        $(this).css("background","red");
+	/*
+	caja.mouseover(function(){
+		$(this).css("background","red");
+	});
 
-    }
+	caja.mouseout(function(){
+		$(this).css("background","green");
+	});
+	*/
 
-    function cambiaNaranja(){
-        $(this).css("background","orange");
+	function cambiaRojo(){
+		$(this).css("background","red");
+	}
 
-    }
+	function cambiaVerde(){
+		$(this).css("background","green");
+	}
 
-    //hover
-    caja.hover(cambiaRojo, cambiaNaranja);
-
-    // click y docble click
-
-    caja.click(function(){
-        $(this).css("background", "blue")
-                .css("color", "white");
-    });
-
-    caja.dblclick(function(){
-        $(this).css("background", "pink")
-                .css("color", "yellow");
-    });
-
-    // focus y blur
-    var nombre =  $("#nombre");
-    var datos = $("#datos");
-
-    nombre.focus(function(){
-        $(this).css("border","2px solid green");
-    });
-    nombre.blur(function(){
-        $(this).css("border","1px solid #ccc");
-        //coje datos del input de formulkario
-        //::>>  $(this).val();
-        datos.text($(this).val()).show();
-    });
-    // mousedown y mouseup
-
-    datos.mousedown(function(){
-        $(this).css("border-color", "gray");
-    });
-
-    datos.mouseup(function(){
-        $(this).css("border-color", "orange");
-    });
-
-        //mousemove
-    $(document).mousemove(function(){
-        console.log("En X: "+event.clientX);
-        console.log("En Y:" +event.clientY);
-            // siguimiento de raton
-            var sigueme =  $("#sigueme");
-            // ocultar mouse
-            //$('body').css("cursor","none");
-            // .........
-            sigueme.css("left", event.clientX);
-            sigueme.css("top", event.clientY);
-    });
+	// Hover
+	caja.hover(cambiaRojo, cambiaVerde);
 
 
+	// Click, Doble click
+	caja.click(function(){
+		$(this).css("background", "blue")
+			   .css("color", "white");
+	});
 
+	caja.dblclick(function(){
+		$(this).css("background", "pink")
+			   .css("color", "yellow");
+	});
+
+	// Focus y blur
+	var nombre = $("#nombre");
+	var datos = $("#datos");
+
+	nombre.focus(function(){
+		$(this).css("border", "2px solid green");
+	});
+
+	nombre.blur(function(){
+		$(this).css("border", "1px solid #ccc");
+
+		datos.text($(this).val()).show();
+	});
+
+	// Mousedown y mouseup
+	datos.mousedown(function(){
+		$(this).css("border-color", "gray");
+	});
+
+	datos.mouseup(function(){
+		$(this).css("border-color", "black");
+	});
+
+	// Mousemove
+	$(document).mousemove(function(){
+		$('body').css("cursor","none");
+		$("#sigueme").css("left", event.clientX)
+		             .css("top", event.clientY);
+	});
 
 });
-
--- Jquery/js/03-textos.js
+++ Jquery/js/03-textos.js
@@ -1,21 +1,26 @@
 $(document).ready(function(){
-    reloadLinks();
-    $('#add_button').click(function(){
-        $('#menu').prepend('<li><a href=" '+$('#add_link').val()+'"></a></li>');
-        $('#add_link').val('');
-        reloadLinks();
-    });
 
-  
+
+	reloadLinks();
+
+	$('#add_button')
+		.removeAttr('disabled')
+		.click(function(){
+			$('#menu').prepend('<li><a href="'+$("#add_link").val()+'"></a></li>');
+
+			$("#add_link").val('');
+			reloadLinks();
+		});
+
 });
 
 function reloadLinks(){
+	$('a').each(function(index){
+		var that = $(this);
+		var enlace = that.attr("href");
 
-    $('a').each(function(index){
-        var that = $(this);
-       var enlace = that.attr("href");
-       that.text(enlace);
-        that.attr('target','_blank');
+		that.attr('target','_blank');
 
-    });
+		that.text(enlace);
+	});
 }
