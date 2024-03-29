a21f00da451e10f6e5fd736a7b338eeb07c62a79
Antonio <antonio@github.com>
2024-03-29 23:44:27 +0100

    update

--- my-first-project/src/src/app/models/configuracion.ts
+++
@@ -1,6 +0,0 @@
-export var Configuracion = {
-	color: "red",
-	fondo: "#eee",
-	titulo: "Master en JavaScript y Angular",
-	descripcion: "Aprendiendo Angular con Víctor Robles - victorroblesweb"
-};
